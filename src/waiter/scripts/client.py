4a1401d2c11c0c97dcc55c5b8393a7911bfc3600
SimoneRosset <rossetti.1900592@studenti.uniroma1.it>
2021-09-05 11:50:42 +0200

    state machine comments added

--- src/waiter/scripts/client.py
+++ src/waiter/scripts/client.py
@@ -76,44 +76,70 @@ def task():
 		c = Customer() # instantiate new customer
 
 		while a!='goodbye':
+			# state machine
 
 			if(a=='welcome'):
+				# welcoming the customer and asks if needs help
+				# flow input: menu/order/checkout/book(maybe -> see book state)
+				# flow output: food description
 				a = im.ask('welcome', timeout=999)
 
 			elif(a=='menu'):
+				# the customer can swipe the foods while pepper
+				# gives a description and the price
+				# flow input: prev/ main page/ next
+				# flow output: food description
 				im.execute(a)
 				im.executeModality('TTS','Fine, let me show what is in our menu!')
 				food_no = 0
 				food_list = list(MENU.keys())
 				while a!='welcome':
 					food = MENU[food_list[food_no]]
-					im.executeModality('TEXT',food.name.upper() + ' - '+ food.price)
+					im.executeModality('TEXT',food.name.upper() + ' - '+ food.price + '<br><i><font size="3">'+ food.description + '</font></i>')
 					im.executeModality('IMAGE',food.img_path)
-					im.executeModality('TTS',food.description)
 					im.executeModality('BUTTONS', [('prev', 'Previous'),  ('welcome', 'Main Page'), ('next', 'Next')])
 					im.executeModality('ASR',{'next': ['next', 'go on', 'next dish'], 'prev': ['previous', 'go back','previous dish'], 'welcome': ['main page', 'go main page','go home', 'stop']})
+					im.executeModality('TTS',food.description)
 					# im.robot.bm_service = im.robot.session.service("ALBackgroundMovement") # gives error
 					# im.executeModality('GESTURE',random.choice(GESTURES)) # gives error
 					a = im.ask(None, timeout=999)
 					if (a=='next'): food_no+=1
 					elif (a=='prev'): food_no-=1
-					food_no = min(len(food_list)-1,max(0,food_no))
+					food_no = food_no % len(food_list)
 
 			elif(a=='order'):
+				# gives a summary of the food ordered then asks 
+				# the type of payment or may display a fake QR code 
+				# saying to scan and go paying, after few seconds 
+				# shows a success page and thanks the customer
+				# flow input: go pay
+				# flow output: success page and thanks
 				a = im.ask(a, timeout=999)
 
 			elif(a=='checkout'):
+				# gives a summary of the food ordered then asks 
+				# the type of payment or may display a fake QR code 
+				# saying to scan and go paying, after few seconds 
+				# shows a success page and thanks the customer
+				# flow input: go pay
+				# flow output: success page and thanks
 				a = im.ask(a, timeout=999)
 
 			elif(a=='book'):
+				# thinking if it is the case to make it, may be time wasting
+				# flow input: date, hour, number of people
+				# flow output: reservation id (Customer.id)
 				a = im.ask(a, timeout=999)
 
 			elif(a=='review'):
+				# flow input: 1 - 5 satisfaction rating
 				a = im.ask(a, timeout=999)
 			
 			elif(a=='goodbye'):
+				# flow input: confirmation/ give a rating/ main page
 				a = im.ask(a, timeout=999)
-				time.sleep(4)
+				if(a=='goodbye'): # goodbye confirmation then wait for the customer to go away
+					time.sleep(4)
 
 			HISTORY[c.id] = c
 
