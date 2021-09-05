4589c8b056cda078de0747d257b2e46ff6675840
SimoneRosset <rossetti.1900592@studenti.uniroma1.it>
2021-09-05 10:51:00 +0200

    state machine added

--- src/waiter/scripts/client.py
+++ src/waiter/scripts/client.py
@@ -12,7 +12,9 @@ from ws_client import *
 
 def task():
 
-	import csv
+	import csv, random
+
+	GESTURES = ['animations/Stand/Gestures/Hey_'+str(i) for i in range(1,6)]
 
 	class Food(object):
 		def __init__(self, name, img_path, price, description):
@@ -47,160 +49,74 @@ def task():
 
 	while True:
 		im.init()
-		flagP = False
-		detected = False
+		detected_person = False
+		detected_person_confirm = False
 		im.executeModality('TEXT_default','Waiting for a human!')
 
 		#Checking if human stay in front of Pepper more than 2 seconds
 		im.robot.startSensorMonitor()
-		while not flagP:
-			while not detected:
+		while not detected_person:
+			while not detected_person_confirm:
 				p = im.robot.sensorvalue() # p is the array with data of all the sensors
-				detected = p[1] > 0.0 and p[1] < 1.0 # p[1] is the Front sonar
-			if detected:
-				im.executeModality('TEXT_default','Hi!')
-				print('*Person Detected*')
+				detected_person_confirm = p[1] > 0.0 and p[1] < 1.0 # p[1] is the Front sonar
+			if detected_person_confirm:
+				print('*Person detected_person_confirm*')
 				time.sleep(2)
 				p = im.robot.sensorvalue()
-				detected = p[1] > 0.0 and p[1] < 1.0
-				if detected:
+				detected_person_confirm = p[1] > 0.0 and p[1] < 1.0
+				if detected_person_confirm:
 					print('*Person still there*')
-					flagP = True
+					detected_person = True
 				else:
 					print('*Person gone*')
 		
 		im.robot.stopSensorMonitor()
 
-		im.execute('welcome')
-		a = im.ask('welcome', timeout=999)
-		c = Customer()
+		a = 'welcome'
+		c = Customer() # instantiate new customer
 
-		if(a=='menu'):
-			im.execute(a)
-			im.executeModality('TTS','Fine, let me show what is in our menu!')
-			food_no = 0
-			food_list = list(MENU.keys())
-			while a!='main':
-				food = MENU[food_list[food_no]]
-				im.executeModality('TEXT',food.name.upper() + ' - '+ food.price)
-				im.executeModality('IMAGE',food.img_path)
-				im.executeModality('TTS',food.description)
-				a = im.ask('menu', timeout=999)
-				if (a=='next'): food_no+=1
-				elif (a=='prev'): food_no-=1
-				food_no = min(len(food_list)-1,max(0,food_no))
+		while a!='goodbye':
 
-		elif(a=='order'):
-			im.ask(a, timeout=999)
+			if(a=='welcome'):
+				a = im.ask('welcome', timeout=999)
 
-		elif(a=='checkout'):
-			im.ask(a, timeout=999)
+			elif(a=='menu'):
+				im.execute(a)
+				im.executeModality('TTS','Fine, let me show what is in our menu!')
+				food_no = 0
+				food_list = list(MENU.keys())
+				while a!='welcome':
+					food = MENU[food_list[food_no]]
+					im.executeModality('TEXT',food.name.upper() + ' - '+ food.price)
+					im.executeModality('IMAGE',food.img_path)
+					im.executeModality('TTS',food.description)
+					im.executeModality('BUTTONS', [('prev', 'Previous'),  ('welcome', 'Main Page'), ('next', 'Next')])
+					im.executeModality('ASR',{'next': ['next', 'go on', 'next dish'], 'prev': ['previous', 'go back','previous dish'], 'welcome': ['main page', 'go main page','go home', 'stop']})
+					# im.robot.bm_service = im.robot.session.service("ALBackgroundMovement") # gives error
+					# im.executeModality('GESTURE',random.choice(GESTURES)) # gives error
+					a = im.ask(None, timeout=999)
+					if (a=='next'): food_no+=1
+					elif (a=='prev'): food_no-=1
+					food_no = min(len(food_list)-1,max(0,food_no))
 
-		elif(a=='book'):
-			im.ask(a, timeout=999)
+			elif(a=='order'):
+				a = im.ask(a, timeout=999)
 
-		if (not a=='main'):
-			a = im.ask('goodbye', timeout=999)
+			elif(a=='checkout'):
+				a = im.ask(a, timeout=999)
 
-		if (a=='review'):
-			im.ask(a, timeout=999)
-		
-		if (not a=='main'):
-			time.sleep(4)
-			flagP = True
+			elif(a=='book'):
+				a = im.ask(a, timeout=999)
 
-		HISTORY[c.id] = c
-
-		# flag = False
-		# flag2 = False
-		# flagP = False
-		# detected = False
-
-		# im.executeModality('TEXT_default','Waiting for a human')
-
-		# #Checking if human stay in front of Pepper more than 2 seconds
-		# im.robot.startSensorMonitor()
-		# while not flagP:
-		# 	while not detected:
-		# 		p = im.robot.sensorvalue() #p is the array with data of all the sensors
-		# 		detected = p[1] > 0.0 and p[1] < 1.0 #p[1] is the Front sonar
-		# 	if detected:
-		# 		print('*Person Detected*')
-		# 		time.sleep(2)
-		# 		p = im.robot.sensorvalue()
-		# 		detected = p[1] > 0.0 and p[1] < 1.0
-		# 		if detected:
-		# 			print('*Person still there*')
-		# 			flagP = True
-		# 		else:
-		# 			print('*Person gone*')
-		# im.robot.stopSensorMonitor()
-
-		# #Starting the script when human stays 2+ seconds
-		# im.execute('welcome')
-		# a0 = im.ask('welcome', timeout=999)
-
-		# #If it is the first time for the visitor, explain history	
-		# if a0 == 'story':
-		# 	im.execute(a0)
-		# 	im.execute('introtour')
-		# 	t0 = im.ask('introtour',timeout=999)
+			elif(a=='review'):
+				a = im.ask(a, timeout=999)
 			
-		# 	#Ask if the new visitor wants a tour
-		# 	if t0 == 'tour':
-		# 		im.execute('tour1')
-		# 		im.execute('tour1map')
-		# 		time.sleep(3)
-		# 		im.execute('tour2')
-		# 		im.execute('tour2map')
-		# 		time.sleep(3)
-		# 		im.execute('tour3')
-		# 		im.execute('tour3map')
-		# 		time.sleep(3)
+			elif(a=='goodbye'):
+				a = im.ask(a, timeout=999)
+				time.sleep(4)
 
-		# else:
-		# 	im.executeModality('TTS','Fine, we have an expert here!')
+			HISTORY[c.id] = c
 
-		# #Main loop of the project, it returns over the map to ask for works
-		# while not flag:
-
-		# 	#If it is the first time that the user view the map, some specific words are shown
-		# 	if not flag2: 		
-		# 		im.executeModality('TTS','This is the full map of the museum. Are you interested in any particular work?')
-		# 		im.executeModality('TEXT_default','Are you interested in any particular work?')	
-		# 		flag2 = True	
-		# 	#Otherwise Pepper TTS and the html change
-		# 	else:
-		# 		im.executeModality('TEXT_default','Are you interested in any other work?')
-		# 		im.executeModality('TTS','Are you interested in any other work?')
-					
-		# 	a = im.ask('fullmap',timeout=5)
-
-		# 	while a=='timeout':
-		# 		im.executeModality('TTS','I did not understand, can you repeat please? You can also use buttons on the tablet.')
-		# 		a = im.ask('fullmap',timeout=5)
-		# 		if flag2:			
-		# 			im.executeModality('TEXT_default','Are you interested in any other work?')
-		# 			im.executeModality('TTS','Are you interested in any other work?')
-
-		# 	#Checking if user want to stop the trip
-		# 	if a=='goodbye':
-		# 		flag=True
-		# 		im.execute('goodbye')
-		# 		break		
-		# 	else:
-		# 		im.execute(a)
-		# 		a2 = im.ask(a,timeout=999)
-				
-		# 		#Checking if user does not want directions				
-		# 		if a2=='fullmap':		
-		# 			im.execute(a2)
-		# 		else: 
-		# 			im.execute(a2)
-		# 			time.sleep(4)	
-		# 			im.execute('fullmap')
-			
-		# time.sleep(6)
 
 if __name__ == "__main__":
 	mws = ModimWSClient()
